3ac36ea77621765e809b57b5388c7ec2168644ee
nezirii <izakneziri@gmail.com>
2019-08-10 16:50:04 -0700

    I have tried to install the package but it will not let me library it.

--- Soil Phosphorous.R
+++ Soil Phosphorous.R
@@ -17,12 +17,19 @@ install.packages("lmerTest")
 install.packages("dplyr")
 install.packages("nortest")
 install.packages("ggplot2")
+install.packages("multcomp")
+install.packages("MuMIn")
+install.packages("emmeans")
+
 library(nlme)
 library(lme4)
 library(lmerTest)
 library(dplyr)
 library(nortest)
 library(ggplot2)
+library(multcomp)
+library(MuMIn)
+library(emmeans)
 
 #Look at mixed effects model
 #start without random factor
@@ -432,4 +439,79 @@ ad.test(residuals(M1.11))
 
 #Log does not work for M1 or M2
 #Log of the 5th root does not work for M1 or M2
+
+#####################################################
+#Get Full Model Statistics and Make Graph
+#####################################################
+#final model
+M.full<-lme(log.P.5th ~ impact*f.time, 
+            random=~ 1 | location, na.action=na.omit, data=sm, weights=vf11)
+
+anova(M.full)
+
+#this extracts what you need to look at pairwise differences and make a graphic
+M.full.em = emmeans(M.full, ~ impact | f.time)
+
+#this shows each pairwise difference (high v. low budworm at each sample event
+pairs(M.full.em)
+
+#the next several lines are building a table you can use in ggplot
+xx = as.data.frame(summary(M.full.em))[c('emmean', 'SE')]
+
+impact = rep((letters[seq(from = 1, to = 2)]), 10)
+impact<-recode(impact, "a" ="High")
+impact<-recode(impact, "b" ="Low")
+event = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10)
+
+log.P.5th.emm = data.frame(cbind(xx,impact,event))
+log.P.5th.emm$emmean.raw = 10^(NTF.DIN.flux.emm$emmean)-1
+log.P.5th.emm$SE.raw = 10^(NTF.DIN.flux.emm$SE)-1
+
+#this is the final table you can use for plotting
+log.P.5th.emm
+
+x = log.P.5th.emm
+
+#make a new vector with the categorical times.  you'll need to adjust this 
+#for your soil graphics
+cat.time<-c("11Sep15", "11Sep15", "11Oct15", "11Oct15", "29Oct15", "29Oct15", "8Nov15", "8Nov15", "8May16", "8May16", "22May16", "22May16", "21Jun16", "21Jun16", "13Jul16", "13Jul16", "21Jul16", "21Jul16", "19Sep16", "19Sep16")
+#force the new vector to be characters
+x$cat.time<-as.character(cat.time)
+#force the new vector to be ordered in the order you gave it instead of alphabetical
+x$cat.time<-factor(x$cat.time, levels=unique(x$cat.time))
+
+pd=position_dodge(0.1)
+
+ggplot(data=x, 
+       aes(x=cat.time, y=emmean.raw, fill=budworm)) + 
+  geom_bar(stat="identity", position=position_dodge(), color = "black") + 
+  geom_errorbar(aes(ymin=emmean.raw, ymax=emmean.raw-SE.raw), width=0.2, 
+                position=position_dodge(0.9)) + 
+  scale_fill_manual(values=c("black","white")) +
+  xlab("Sample Event") +
+  ylab(expression(DIN~net~throughfall~flux~(kg~N~ha^{-1}))) +
+  labs(fill="Budworm Activity") +
+  theme_bw() +
+  geom_hline(yintercept=0)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        legend.title=element_text(size=6),
+        legend.key=element_blank(),
+        legend.position=c(0.5,0.95),
+        legend.text=element_text(size=8),
+        legend.background=element_blank(),
+        legend.direction="horizontal",
+        legend.key.size=unit(0.3, "cm"),
+        axis.title.y=element_text(size=8),
+        axis.title.x=element_text(size=8),
+        axis.text.x=element_text(size=8))
+
+
+#this will save the file
+ggsave('figures/emmnetdinTFflux.tiff',
+       units="in",
+       width=5.5,
+       height=4.5,
+       dpi=1200,
+       compression="lzw")
 
