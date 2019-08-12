ca80cbb29b92268c477a3e73418cb89cb93917a3
nezirii <izakneziri@gmail.com>
2019-08-12 11:40:30 -0700

    asdg

--- Soil Ammonia.R
+++ Soil Ammonia.R
@@ -378,3 +378,78 @@ plot(M1.1) #residuals vs fitted values
 plot(x, E1.1)
 
 #1.1 Works but its not quite as good as M1.8 using the base model without the random factor
+
+#####################################################
+#Get Full Model Statistics and Make Graph
+#####################################################
+#final model
+M.full<-lme(log.NH4 ~ impact+f.time, 
+            random=~ 1 | location, na.action=na.omit, data=sm, weights=vf1)
+
+anova(M.full)
+
+#this extracts what you need to look at pairwise differences and make a graphic
+M.full.em = emmeans(M.full, ~ impact | f.time)
+
+#this shows each pairwise difference (high v. low budworm at each sample event
+pairs(M.full.em)
+
+#the next several lines are building a table you can use in ggplot
+xx = as.data.frame(summary(M.full.em))[c('emmean', 'SE')]
+
+impact = rep((letters[seq(from = 1, to = 2)]), 10)
+impact<-recode(impact, "a" ="High")
+impact<-recode(impact, "b" ="Low")
+event = c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10)
+
+log.NH4.emm = data.frame(cbind(xx,impact,event))
+log.NH4.emm$emmean.raw = 10^(log.NH4.emm$emmean)-1
+log.NH4.emm$SE.raw = 10^(log.NH4.emm$SE)-1
+
+#this is the final table you can use for plotting
+log.NH4.emm
+
+x = Log.NH4.emm
+
+#make a new vector with the categorical times.  you'll need to adjust this 
+#for your soil graphics
+cat.time<-c("11Sep15", "11Sep15", "11Oct15", "11Oct15", "29Oct15", "29Oct15", "8Nov15", "8Nov15", "8May16", "8May16", "22May16", "22May16", "21Jun16", "21Jun16", "13Jul16", "13Jul16", "21Jul16", "21Jul16", "19Sep16", "19Sep16")
+#force the new vector to be characters
+x$cat.time<-as.character(cat.time)
+#force the new vector to be ordered in the order you gave it instead of alphabetical
+x$cat.time<-factor(x$cat.time, levels=unique(x$cat.time))
+
+pd=position_dodge(0.1)
+
+ggplot(data=x, 
+       aes(x=cat.time, y=emmean.raw, fill=budworm)) + 
+  geom_bar(stat="identity", position=position_dodge(), color = "black") + 
+  geom_errorbar(aes(ymin=emmean.raw, ymax=emmean.raw-SE.raw), width=0.2, 
+                position=position_dodge(0.9)) + 
+  scale_fill_manual(values=c("black","white")) +
+  xlab("Sample Event") +
+  ylab(expression(DIN~net~throughfall~flux~(kg~N~ha^{-1}))) +
+  labs(fill="Budworm Activity") +
+  theme_bw() +
+  geom_hline(yintercept=0)+
+  theme(panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),
+        legend.title=element_text(size=6),
+        legend.key=element_blank(),
+        legend.position=c(0.5,0.95),
+        legend.text=element_text(size=8),
+        legend.background=element_blank(),
+        legend.direction="horizontal",
+        legend.key.size=unit(0.3, "cm"),
+        axis.title.y=element_text(size=8),
+        axis.title.x=element_text(size=8),
+        axis.text.x=element_text(size=8))
+
+
+#this will save the file
+ggsave('figures/emmnetdinTFflux.tiff',
+       units="in",
+       width=5.5,
+       height=4.5,
+       dpi=1200,
+       compression="lzw")
